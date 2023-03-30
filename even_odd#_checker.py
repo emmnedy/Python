abddcc3456fa0ebeaa42fd7dc20572631a0df882
emmnedy <115366201+emmnedy@users.noreply.github.com>
2023-03-30 00:12:17 -0400

    Create even_odd#_checker.py
    This tiny Python program check  if number entered is Even or Odd Number .
    
    # NueL

---
+++ even_odd#_checker.py
@@ -0,0 +1,5 @@
+numero = int(input("Enter Number to be Tested: "))
+if (numero % 2) == 0:
+   print(f"this number : {numero} : is an EVEN number ")
+else :
+   print(f"this number : {numero} : is an ODD number ")
